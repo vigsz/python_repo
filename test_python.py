ca23716820cfc9346013690c8412a90dc2e83080
Szilard Vig <vigszilard@gmail.com>
2020-05-28 19:46:01 +0300

    4th commit

--- test_python.py
+++ test_python.py
@@ -1,19 +1,19 @@
 print('Hello')
 
-# one_variable = '5'
-# some_string = 'Some other string'
+one_variable = '5'
+some_string = 'Some other string'
 
-# third_variable = one_variable + some_string
+third_variable = one_variable + some_string
 
-# some_number = 5
-# some_other_number = 6
+some_number = 5
+some_other_number = 6
 
-# result = some_number + some_other_number
+result = some_number + some_other_number
 
-# items = ['1', '2', '3', '5']
+items = ['1', '2', '3', '5']
 
-# for item in items:
-#     print(item)
+for item in items:
+    print(item)
 
 numbers = [1, 2, 3, 4, 5, 6]
 
