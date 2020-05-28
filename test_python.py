346767a9fd08c5e3fecccf5d8c3a3cf7afaa1d66
Szilard Vig <vigszilard@gmail.com>
2020-05-28 19:04:29 +0300

    second commit

--- test_python.py
+++ test_python.py
@@ -1,5 +1,22 @@
-print("Hello World")
+print('Hello')
 
-print("New Line")
+# one_variable = '5'
+# some_string = 'Some other string'
 
-print("New line again")
+# third_variable = one_variable + some_string
+
+# some_number = 5
+# some_other_number = 6
+
+# result = some_number + some_other_number
+
+# items = ['1', '2', '3', '5']
+
+# for item in items:
+#     print(item)
+
+numbers = [1, 2, 3, 4, 5, 6]
+
+for number in numbers:
+    if number % 2 == 0:
+        print(number)
